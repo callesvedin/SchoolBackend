0f1ab7c279b44639105df1fe6ff80cd4517fca0b
Carl-Johan Svedin <carl-johan.svedin@infomaker.se>
2017-01-27 12:23:05 +0100

    Added request to school backend to get the request and a tableviewcontroller to display them. Still not connected to the actual tests

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "school",
+    name: "SchoolBackend",
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3),
         .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 1, minor: 1)        
